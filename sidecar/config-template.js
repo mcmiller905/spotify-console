2c2592b04a4469b5f8e68d376441fd72f4e17f96
Mike <mcmiller905@gmail.com>
2023-02-04 15:58:43 -0500

    initial push

---
+++ sidecar/config-template.js
@@ -0,0 +1,10 @@
+// config-template.js
+// copy as config.js and add your personal info
+// =========
+module.exports = {
+    get_client_id: function(){ return 'put-your-client-id-here'; },
+    get_client_secret: function(){ return 'put-your-client-secret-here'; },
+    get_redirect_uri: function () { return 'put-your-redirect-uri-here'; },
+    get_playlist_id: function() { return 'put-playlist-to-add-to-id-here'; },
+    get_radio_playlist_id: function() { return 'put-radio-playlist-id-here';}
+};

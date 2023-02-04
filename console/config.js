7c9632158ab32f5c364c838078e3968a04916825
Mike <mcmiller905@gmail.com>
2023-02-04 16:07:14 -0500

    config

--- console/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-// =========
-module.exports = {
-    get_client_id: function(){ return 'f8c37223834c4fa28974c251fb522bb2'; },
-    get_client_secret: function(){ return '7fe2f589fcbe44cca4b115770c4bcab7'; },
-    get_redirect_uri: function() { return 'http://localhost:8888/callback'; },
-    get_playlist_id: function() { return '6tU6fmirTyUPxjEn3xS4Pc'; },
-    get_radio_playlist_id: function() { return '2gsPu9jxPRjB4ynbWgGwcl';}
-};
